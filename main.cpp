c04a48ff5005afa214f6f5525449e5bac38fbc3e
Depyter <acabalharleyvan@gmail.com>
2024-05-19 22:06:10 +0800

    chore: Update README.md with recipe storage details and file structure

--- main.cpp
+++ main.cpp
@@ -19,6 +19,8 @@ class Recipe {
 // There should be a standard way to display the recipe information and also store the recipe information in the text files.
 // To be written in the Readme file.
 // Possibly use multiple files to store the information.
+// TODO: Use Levenshtein distance to search for recipes based on similar keywords or recipe names.
+// Use maps in c++ to store the recipe keywords.
 
 int main() {
 
